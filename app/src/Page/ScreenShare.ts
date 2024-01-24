6c476f732501a7bfcb8f32e2ff2752748fdfcc7a
윤서희 <appleid21@sookmyung.ac.kr>
2024-01-24 20:45:46 +0900

    Fix : 자잘한 error 수정

--- app/src/Page/ScreenShare.ts
+++ app/src/Page/ScreenShare.ts
@@ -1,25 +1,23 @@
 import { Scene } from 'phaser';
 import { TagManager } from '../util/TagManager';
-import { Player } from '../characters/Player';
+import { Player } from '../Objects/Player';
 
 const tagManager = TagManager.getInstance();
 
 export const shareScreen = async (scene: Scene, player: Player, mainDiv: HTMLDivElement) => {
   let screenContainer: HTMLDivElement;
-  let peerConnection: RTCPeerConnection;
+  let firstStreamAdded = false;
 
   const handleIceScreen = (data) => {
     console.log('sent screen candidate');
-    scene.room.send("screen_ice", { candidate: data.candidate });
-  };
+    scene.room.send("screen_ice", { candidate: data.candidate, roomName: player.roomName});
+  }
 
   const handleAddStreamScreen = (data) => {
-    console.log('Data stream:', data.stream);
-
     const peersStream = tagManager.createVideo({
       parent: screenContainer,
-      width: 100,
-      height: 80,
+      width: 400,
+      height: 320,
       srcObject: data.stream,
       autoplay: true,
       playsInline: true,
@@ -28,106 +26,158 @@ export const shareScreen = async (scene: Scene, player: Player, mainDiv: HTMLDiv
         'margin-top': '20px',
       }
     });
+  }
 
-    console.log('peersStream:', peersStream.srcObject);
-  };
+  try {
+    const stream = await navigator.mediaDevices.getDisplayMedia({ video: { displaySurface: 'monitor' } });
 
-  navigator.mediaDevices.getDisplayMedia({ video: { displaySurface: 'monitor' } })
-    .then(async (stream) => {
-      console.log('my', stream);
-      const mainContainer = tagManager.createDiv({
-        parent: mainDiv,
-        styles: {
-          'position': 'absolute',
-          'width': '50%',
-          'height': '50%',
-          'background-color': 'rgba(0, 0, 0, 0.4)',
-          'z-index': '1000',
-          'border-radius': '10px',
-        }
-      });
+    const mainContainer = tagManager.createDiv({
+      parent: mainDiv,
+      styles: {
+        'position': 'absolute',
+        'width': '50%',
+        'height': '50%',
+        'background-color': 'rgba(0, 0, 0, 0.4)',
+        'z-index': '1000',
+        'border-radius': '10px',
+      }
+    });
 
-      screenContainer = tagManager.createDiv({
+    screenContainer = tagManager.createDiv({
+      parent: mainContainer,
+      styles: {
+        'display': 'flex',
+        'flex-direction': 'column',
+        'margin': '16px',
+      }
+    });
+
+    const screen = tagManager.createVideo({
+      parent: screenContainer,
+      srcObject: stream,
+      width: 350,
+      height: 280,
+      playsInline: true,
+      autoplay: true,
+      styles: {
+        'margin-top': '20px',
+        'border-radius': '30px',
+      }
+    });
+
+    const inputContainer = tagManager.createDiv({
         parent: mainContainer,
         styles: {
           'display': 'flex',
-          'flex-direction': 'column',
-          'margin': '16px',
+          'flex-direction': 'row',
+          'margin': '20px',
         }
       });
 
-      const myScreen = tagManager.createVideo({
-        parent: screenContainer,
-        srcObject: stream,
-        width: 50,
+    const idInput = tagManager.createInput({
+        parent: inputContainer,
+        width: 200,
         height: 40,
-        playsInline: true,
-        autoplay: true,
+        placeholder: "room name",
         styles: {
-          'margin-top': '20px',
-          'border-radius': '30px',
+          'display': 'flex',
+          'border-radius': '10px',
+          'font-size': '20px',
         }
       });
 
-      const stopSharingButton = tagManager.createButton({
-        parent: mainContainer,
-        text: 'Stop Sharing',
-        onClick: async () => {
-          stream.getTracks().forEach((track: { stop: () => any; }) => track.stop());
-          peerConnection.close();
-          removeVideoElement(screenContainer);
-          tagManager.setVisible(mainContainer, false);
+    const idButton = tagManager.createButton({
+        parent: inputContainer,
+        text: "입장",
+        width: 60,
+        height: 45,
+        onClick: (event) => {
+          event.preventDefault();
+          const roomName = idInput.value;
+          player.roomName = roomName;
+          scene.room.send("join_room_screen", {roomName});
+          idInput.value = "";
+        },
+        styles: {
+          'border-radius': '5px',
+          'margin-left': '10px',
+          'font-size': '20px',
         }
       });
 
-      peerConnection = new RTCPeerConnection();
-      peerConnection.addEventListener("icecandidate", handleIceScreen);
-      peerConnection.addEventListener("addstream", handleAddStreamScreen);
+    const stopSharingButton = tagManager.createButton({
+      parent: mainContainer,
+      text: 'Stop Sharing',
+      onClick: async () => {
+        stream.getTracks().forEach((track: { stop: () => any; }) => track.stop());
+        peerConnection.close();
+        removeVideoElement(screenContainer);
+        tagManager.setVisible(mainContainer, false);
+      }
+    });
 
-      stream.getTracks().forEach((track) => peerConnection.addTrack(track, stream));
+    const peerConnection = new RTCPeerConnection();
+    peerConnection.addEventListener("icecandidate", handleIceScreen);
+    peerConnection.addEventListener("addstream", handleAddStreamScreen);
 
-      const offer = await peerConnection.createOffer();
-      await peerConnection.setLocalDescription(offer);
+    stream.getTracks().forEach((track) => peerConnection.addTrack(track, stream));
 
-      scene.room.send("offer_screen", { offer: offer });
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
 
-      scene.room.onMessage("offer_screen", async (messageData) => {
+    scene.room.onMessage("join_room_screen", async (messageData) => {
+        if(scene.currentPlayer.roomName == messageData.roomName){ // do not needed
+          if(scene.currentPlayer.playerId == messageData.playerId){
+            console.log("its me!!"); 
+          } else{
+            console.log(`join ${messageData.playerId}!!`); 
+            const offer = await peerConnection.createOffer();
+            peerConnection.setLocalDescription(offer);
+            console.log("sent the offer");
+            scene.room.send("offer_screen", {offer: offer, roomName: messageData.roomName});
+          }
+        }
+        return;
+      });
+
+    scene.room.onMessage("offer_screen", async (messageData) => {
+        if(scene.currentPlayer.playerId == messageData.playerId)
+          return;
+        
         console.log("received the screen offer");
-        if (messageData.playerId === scene.currentPlayer.playerId) return;
-
-        console.log(peerConnection.signalingState);
         await peerConnection.setRemoteDescription(messageData.offer);
-
         const answer = await peerConnection.createAnswer();
         await peerConnection.setLocalDescription(answer);
-
         console.log("sent the screen answer");
-        scene.room.send("answer_screen", { answer: answer });
-      });
+        scene.room.send("answer_screen", { answer: answer, roomName: messageData.roomName });
+    });
 
-      scene.room.onMessage("answer_screen", async (messageData) => {
-        console.log("receive the screen answer");
-        if (messageData.playerId === scene.currentPlayer.playerId) return;
-
-        await peerConnection.setRemoteDescription(messageData.answer);
-      });
-
-      scene.room.onMessage("screen_ice", async (messageData) => {
-        if (messageData.playerId == scene.currentPlayer.playerId)
+    scene.room.onMessage("answer_screen", async (messageData) => {
+        if(scene.currentPlayer.playerId == messageData.playerId)
           return;
-        console.log("receive screen candidate");
-        await peerConnection.addIceCandidate(messageData.candidate);
-      });
+        
+        console.log("receive the screen answer");
+        await peerConnection.setRemoteDescription(messageData.answer);
+    });
 
-      peerConnection.oniceconnectionstatechange = async () => {
+    scene.room.onMessage("screen_ice", async (messageData) => {
+        if(scene.currentPlayer.playerId == messageData.playerId)
+          return;  
+        
+        console.log("receive screen candidate");
+        await peerConnection.addIceCandidate(messageData.ice);
+    });
+
+    peerConnection.oniceconnectionstatechange = async () => {
+      if (peerConnection.iceConnectionState === 'closed' || peerConnection.iceConnectionState === 'failed') {
         stream.getTracks().forEach((track) => track.stop());
         mainDiv.removeChild(stopSharingButton);
-        removeVideoElement(screenContainer);
-      };
-    })
-    .catch((error) => {
-      console.error("Error accessing display media:", error);
-    });
+        removeVideoElement(screenContainer); // Pass the container to remove
+      }
+    };
+  } catch (error) {
+    console.error("Error accessing display media:", error);
+  }
 };
 
 function removeVideoElement(container: HTMLDivElement) {
